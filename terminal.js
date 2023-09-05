49ec7427f2959af42d5dff988478d13a68e0150c
Skel-Enkai <williamwarrenmeeks@gmail.com>
2023-09-05 19:15:08 +0000

    I have made a started on learning Javascript, and have structured how the command system will work in the terminal.

--- terminal.js
+++ terminal.js
@@ -2,16 +2,31 @@ $('body').terminal({
     echo: function(arg1) {
         this.echo(arg1);
     },
-    dice: function() {
-        function check() {
-            return 'Working';
-        }
-        this.read('Please Enter the Number of Dice and the Number of Sides in the format 2d6', check())
-    }
+    dice: function(){
+        terminal = this;
+        terminal.read('Please Enter the Number of Dice and the Number of Sides in the format 2d6: ').then(function(string){
+            if (check(string)){
+                terminal.echo('Dice have been updated.\n');
+            } else {
+                terminal.echo('Please retry with the given format. Dice have not been updated.\n');
+            } 
+        });
+
+    },
+    
 }, {
     onInit: function() {
-        this.echo('Hello, to use my binomial dice tool, please type: \'dice\'');
+        this.echo('Hello, and welcome to my BinomialDice tool.');
+        this.echo('There are currently x commands.\n');
         },
     greeting: false,
-    prompt: '>' 
+    prompt: 'diceStats> ' 
 });
+
+var isInt;
+var number_of_dice;
+var number_of_sides;
+
+function check(input) {
+    return true;
+}
